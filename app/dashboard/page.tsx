764f2a5b7d64ba09a6f0447187aac4d69cc85632
abhayrathor280 <123531336+abhayrathor280@users.noreply.github.com>
2026-01-07 23:59:30 +0530

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,16 +1,17 @@
 export const dynamic = "force-dynamic";
-export const revalidate = 0;
 
 import Product from "../../models/Product";
 import { connectDB } from "../../lib/db";
 
 export default async function Dashboard() {
   await connectDB();
+
   const products = await Product.find();
 
   return (
     <div>
-      Dashboard
+      Dashboard Loaded ✅  
+      Products count: {products.length}
     </div>
   );
 }
