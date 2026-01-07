1a93863cec030d78ce086821554a4bfacffce3ed
abhayrathor280 <123531336+abhayrathor280@users.noreply.github.com>
2026-01-07 23:49:51 +0530

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,8 @@
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
-import Product from "@/models/Product";
-import connectDB from "@/lib/db";
+import Product from "../../models/Product";
+import connectDB from "../../lib/db";
 
 export default async function Dashboard() {
   await connectDB();
