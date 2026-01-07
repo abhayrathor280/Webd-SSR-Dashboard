16a4bf347bedce214534ead0b8ab8cd99ff0cf44
abhayrathor280 <123531336+abhayrathor280@users.noreply.github.com>
2026-01-07 23:22:29 +0530

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,16 +1,17 @@
+export const dynamic = "force-dynamic";
+
 import { connectDB } from "../../lib/db";
 import Product from "../../models/Product";
-
 import Chart from "../../components/Chart";
-
 
 export default async function Dashboard() {
   await connectDB();
   const products = await (Product as any).find();
+
   return (
     <div>
-      <h1>Admin Dashboard</h1>
-      <Chart products={JSON.parse(JSON.stringify(products))} />
+      <Chart data={products} />
     </div>
   );
 }
+
